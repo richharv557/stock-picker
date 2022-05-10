5c4e76bab6b0ff7f17f54d042d24a0613c4bdf33
richharv557 <richharv557@gmail.com>
2022-05-10 02:17:35 -0500

    Initial Commit - passes criteria

---
+++ stock_picker.rb
@@ -0,0 +1,25 @@
+def stock_picker(stock_prices)
+  total_days = stock_prices.length - 1 
+  largest_profit = 0
+  target_days = []
+
+  stock_prices.each_with_index do |stock, index|
+    i = index
+    for i in index..total_days
+      current_profit = stock_prices[i] - stock
+      if current_profit > largest_profit
+        largest_profit = current_profit
+        target_days = [index, i]
+      end
+      i+= 1
+    end
+  end
+  p target_days
+end
+
+stock_picker([100,3,6,9,15,8,6,1,0])
+
+# for each day, loop the hypothetical sale of that stock through the remaining days. 
+# when the largest difference is found, return the start index and end index
+#progress 
+
