453321bf6c753500362d05d1c9740d0936ad29fa
richharv557 <richharv557@gmail.com>
2022-05-13 22:14:01 -0500

    removed for loop, replaced with each_with_index

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,21 +4,16 @@ def stock_picker(stock_prices)
   total_days = stock_prices.length - 1
   largest_profit = 0
   target_days = []
-  stock_prices.each_with_index do |stock, index|
-    for i in index..total_days
-      current_profit = stock_prices[i] - stock
+  stock_prices.each_with_index do |_stock, index|
+    stock_prices[index..total_days].each_with_index do |day_value, i|
+      current_profit =  day_value - stock_prices[index]
       if current_profit > largest_profit
         largest_profit = current_profit
-        target_days = [index, i]
+        target_days = [index, i + index]
       end
-      i += 1
     end
   end
   p target_days
 end
 
 stock_picker([100, 3, 6, 9, 15, 8, 6, 1, 0])
-
-# for each day, loop the hypothetical sale of that stock through the remaining days.
-# when the largest difference is found, return the start index and end index
-# progress
